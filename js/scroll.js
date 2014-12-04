d2a340aa6fa17d2505edecd51fd309e464d6c91c
unknown <x14328981@STLAB14-15-0058.student.nci.col>
2014-12-04 00:02:54 +0000

    merged files

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -30,36 +29,3 @@ $(document).ready(function(){
 	});
 	
 });
-=======
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-     'scrollTop': $target.offset().top
-}, 900, 'swing');	 
-	       
-	});
-});		 
-$(document).ready(function(){
-	
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-	
-});
->>>>>>> 36aea27f375cc542ec7c317735412cfb5a19c946
